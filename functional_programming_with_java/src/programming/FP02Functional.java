817cf1fa75a344a425b652cf100c043a8b392fcd
sniperzving <114675858+sniperzving@users.noreply.github.com>
2023-06-15 13:03:56 +0200

    learn-java-functional-programming-with-lambdas-and-streams -> Adding reduce method (Min MAx)

--- functional_programming_with_java/src/programming/FP02Functional.java
+++ functional_programming_with_java/src/programming/FP02Functional.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class FP02Functional {
-	private static final List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
-	private static final List<Integer> numbersList = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-	
+	private static final List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF",
+			"Azure", "Docker", "Kubernetes");
+	private static final List<Integer> numbersList = List.of(-12, 12, 9, 13, 4, 6, 2, 4, 12, 15);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -21,43 +22,58 @@ public class FP02Functional {
 		int sum = addListFunctional(numbersList);
 		System.out.println(sum);
 		System.out.println("\n\n");
+		int max = addMaxListFunctional(numbersList);
+		System.out.println(max);
+		System.out.println("\n\n");
+		int min = addMinListFunctional(numbersList);
+		System.out.println(min);
+		System.out.println("\n\n");
+	}
+
+	private static int addMaxListFunctional(List<Integer> numberslist2) {
+
+		return numberslist2.stream().reduce(Integer.MIN_VALUE, (x, y) -> x > y ? x : y);
+	}
+
+	private static int addMinListFunctional(List<Integer> numberslist2) {
+
+		return numberslist2.stream().reduce(Integer.MAX_VALUE, (x, y) -> x < y ? x : y);
 	}
 
 	private static int sum(int a, int b) {
-		System.out.println(a+"  "+b);
-		return a+b;
+		System.out.println(a + "  " + b);
+		return a + b;
 	}
-	
+
 	private static int addListFunctional(List<Integer> numberslist2) {
 
-		
 //		return numberslist2.stream().reduce(0, FP02Functional::sum);
 //		return numberslist2.stream().reduce(0, Integer::sum);
-		return numberslist2.stream().reduce(0, (a, b) -> a+b);
+		return numberslist2.stream().reduce(0, (a, b) -> a + b);
 	}
 
 	private static void printAllCoursesInListFunctional(List<String> courses) {
 		// TODO Auto-generated method stub
 		courses.stream().forEach(course -> System.out.println(course));
-		
+
 	}
-	
+
 	private static void printAllCoursesContainingSpringInListFunctional(List<String> courses) {
 		// TODO Auto-generated method stub
 		courses.stream().filter(course -> course.contains("Spring")).forEach(System.out::println);
-		
+
 	}
-	
+
 	private static void printAllCoursesWithFourLettersInListFunctional(List<String> courses) {
 		// TODO Auto-generated method stub
-		courses.stream().filter(course -> course.length()>=4).forEach(System.out::println);
-		
+		courses.stream().filter(course -> course.length() >= 4).forEach(System.out::println);
+
 	}
-	
+
 	private static void printCaractersNumbersOfEachCourseInListFunctional(List<String> courses) {
 		// TODO Auto-generated method stub
-		courses.stream().map(course -> course + " ---> "+course.length()).forEach(System.out::println);
-		
+		courses.stream().map(course -> course + " ---> " + course.length()).forEach(System.out::println);
+
 	}
 
 }
